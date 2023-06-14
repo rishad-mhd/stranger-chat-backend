d83807586f689f8bda22c0106637e79863682a88
rashid <rashidrashi028@gmail.com>
2023-06-14 13:12:39 +0530

    timestamps added to the user

--- models/User.js
+++ models/User.js
@@ -1,17 +1,20 @@
 const { Schema, default: mongoose } = require("mongoose");
 
-const userSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+    },
+    gender: { type: String, enum: ["male", "female", "other"] },
+    avatar: String,
   },
-  name: {
-    type: String,
-  },
-  gender: { type: String, enum: ["male", "female", "other"] },
-  avatar: String,
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
